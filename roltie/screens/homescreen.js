e37f02184b7d1ce74c7e0a13e22b555a08cf4bbf
NOAHDESMIT <n.desmit@live.nl>
2024-05-28 13:15:25 +0200

    ik heb de homescreen aangemaakt en een beetje gestyled

---
+++ roltie/screens/homescreen.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Image source={require('./assets/logo.png')} style={styles.logo} />
+            <View style={styles.rectangle}>
+                <TextInput placeholder="Van" style={styles.input} />
+                <TextInput placeholder="Naar" style={styles.input} />
+                <TouchableOpacity style={styles.button} onPress={() => {}}>
+                    <Text style={styles.buttonText}>Roltie</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#4A4A4A',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    logo: {
+        position: 'absolute',
+        top: '4%',
+        width: 200,
+        height: 100,
+        resizeMode: 'contain',
+    },
+    rectangle: {
+        width: '100%',
+        height: '84%',
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        paddingTop: 50,
+    },
+    input: {
+        height: 40,
+        width: '80%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+    },
+    button: {
+        backgroundColor: '#00C720', // Groene kleur
+        paddingVertical: 12,
+        paddingHorizontal: 40,
+        borderRadius: 5,
+        alignItems: 'center',
+        width: '80%',
+    },
+    buttonText: {
+        color: '#FFFFFF', // Witte tekst
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
