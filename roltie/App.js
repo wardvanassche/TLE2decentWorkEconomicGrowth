ec1e38172d60438d421cbf8815a54f246604e5f1
Ward van Asschegit config --global user.email 1060834@hr.nl <1060834@hr.nl>
2024-06-04 13:54:52 +0200

    dropdown done

--- roltie/App.js
+++ roltie/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/homescreen';
-import MeldingScreen from "./screens/meldingscreen";
+import HomeScreen from './screens/HomeScreen';
+import MeldingScreen from "./screens/MeldingScreen";
 
 export default function App() {
   return (
-      <MeldingScreen />
+      <MeldingScreen/>
   );
 }
