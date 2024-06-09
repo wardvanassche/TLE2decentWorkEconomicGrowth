99b7ccc7a398052fa6fd217f0c59c22f498d8359
Rayel2002 <1002893@hr.nl>
2024-06-09 23:47:01 +0200

    installed brain.js

--- roltie/App.js
+++ roltie/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/homescreen';
-import MeldingScreen from "./screens/meldingscreen";
+import HomeScreen from './screens/HomeScreen.js';
+import MeldingScreen from "./screens/MeldingScreen.js";
 import TijdelijkeMeldingen from "./screens/tijdelijkeMeldingen.js";
 
 export default function App() {
